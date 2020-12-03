3a38857b06e8fa4009414ceb52afc656853566e5
eng wafaa <wafaajame@gmail.com>
2020-12-03 19:25:42 +0200

    Make reset password layout

---
+++ app/src/main/java/com/ramez/shopp/Adapter/InvoiceItemAdapter.java
@@ -0,0 +1,236 @@
+package com.ramez.shopp.Adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+import com.ramez.shopp.Classes.CartModel;
+import com.ramez.shopp.R;
+import com.ramez.shopp.Utils.NumberHandler;
+import com.ramez.shopp.databinding.RowInvoiceProductItemBinding;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class InvoiceItemAdapter extends RecyclerSwipeAdapter<InvoiceItemAdapter.Holder> {
+
+    private static final String TAG = "Log CartAdapter";
+    public int count;
+    public String currency;
+    private Context context;
+    private List<CartModel> cartDMS;
+    private OnInvoiceItemClicked onInvoiceItemClicked;
+
+
+
+    public InvoiceItemAdapter(Context context, List<CartModel> cartDMS, OnInvoiceItemClicked onInvoiceItemClicked) {
+        this.context = context;
+        this.cartDMS = cartDMS;
+        this.onInvoiceItemClicked = onInvoiceItemClicked;
+
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RowInvoiceProductItemBinding itemView = RowInvoiceProductItemBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new Holder(itemView);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(final Holder holder, int position) {
+        CartModel cartDM = cartDMS.get(position);
+        currency = "AED";
+        holder.binding.productCartQTY.setText(cartDM.getProductQuantity() + "");
+        holder.binding.productName.setText(cartDM.getProductName());
+
+        holder.binding.cardViewOuter.setOnClickListener(v -> {
+            Log.d(TAG, "name p" + cartDM.getProductName());
+            Log.d(TAG, "ModelID p" + cartDM.getModelID());
+
+            onInvoiceItemClicked.onInvoiceItemClicked(cartDM);
+        });
+
+        holder.binding.productImage.setOnClickListener(v -> {
+            Log.d(TAG, "name p" + cartDM.getProductName());
+
+            onInvoiceItemClicked.onInvoiceItemClicked(cartDM);
+        });
+
+
+        if (cartDM.getProductPrice().isEmpty()) {
+            holder.binding.productPriceTv.setVisibility(View.GONE);
+            Log.d("proImage", "cart proImage: " + cartDM.getProductImage());
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), Uri.parse(cartDM.getProductImage()));
+                Glide.with(context).load(bitmap).placeholder(R.drawable.holder_image).into(holder.binding.productImage);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        else {
+            double subTotal = Double.parseDouble(cartDM.getProductPrice()) * cartDM.getProductQuantity();
+            Glide.with(context).load(cartDM.getProductImage()).placeholder(R.drawable.holder_image).into(holder.binding.productImage);
+
+        }
+
+
+        calculateSubTotalPrice();
+
+        if (Integer.parseInt(holder.binding.productCartQTY.getText().toString()) == 1) {
+
+            holder.binding.deleteCartBtn.setVisibility(View.VISIBLE);
+            holder.binding.minusCartBtn.setVisibility(View.GONE);
+        } else {
+            holder.binding.minusCartBtn.setVisibility(View.VISIBLE);
+            holder.binding.deleteCartBtn.setVisibility(View.GONE);
+
+        }
+
+
+        holder.binding.swipe.setShowMode(SwipeLayout.ShowMode.LayDown);
+        holder.binding.swipe.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+
+            }
+        });
+    }
+
+    public double calculateSubTotalPrice() {
+        double subTotal = 0;
+        for (int i = 0; i < cartDMS.size(); i++) {
+            if (!cartDMS.get(i).getProductPrice().isEmpty()) {
+                subTotal += Double.parseDouble(cartDMS.get(i).getProductPrice()) * cartDMS.get(i).getProductQuantity();
+            }
+        }
+
+        return subTotal;
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return cartDMS.size();
+    }
+
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+    }
+
+    public interface OnInvoiceItemClicked {
+        void onInvoiceItemClicked(CartModel cartDM);
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+
+        RowInvoiceProductItemBinding binding;
+
+        @SuppressLint("SetTextI18n")
+        Holder(RowInvoiceProductItemBinding view) {
+            super(view.getRoot());
+            binding = view;
+
+            binding.deleteCartBtn.setOnClickListener(view1 -> {
+                int position = getAdapterPosition();
+
+            });
+
+            binding.plusCartBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CartModel cartDM = cartDMS.get(getAdapterPosition());
+                    count = Integer.parseInt(binding.productCartQTY.getText().toString());
+                    if (count == 1) {
+                        binding.minusCartBtn.setVisibility(View.VISIBLE);
+                        binding.deleteCartBtn.setVisibility(View.GONE);
+                    }
+                    count++;
+                    binding.productCartQTY.setText(String.valueOf(count));
+
+                    if(!cartDM.getProductPrice().isEmpty()){
+                        double newSubTotal = Double.parseDouble(cartDM.getProductPrice()) * count;
+                        binding.productPriceTv.setText(NumberHandler.formatDouble(newSubTotal) + " " + currency);
+
+                    }
+
+                    count = Integer.parseInt(binding.productCartQTY.getText().toString());
+                    cartDM.setProductQuantity(count);
+
+
+                }
+            });
+
+            binding.minusCartBtn.setOnClickListener(v -> {
+                CartModel cartDM = cartDMS.get(getAdapterPosition());
+                count = Integer.parseInt(binding.productCartQTY.getText().toString());
+                binding.productCartQTY.setText(String.valueOf(count));
+
+                if (count == 1) {
+                    binding.minusCartBtn.setVisibility(View.GONE);
+                    binding.plusCartBtn.setVisibility(View.VISIBLE);
+                    binding.deleteCartBtn.setVisibility(View.VISIBLE);
+
+                } else {
+                    binding.minusCartBtn.setVisibility(View.VISIBLE);
+                    binding.plusCartBtn.setVisibility(View.VISIBLE);
+                    binding.deleteCartBtn.setVisibility(View.GONE);
+                    count--;
+
+                }
+
+                binding.productCartQTY.setText(String.valueOf(count));
+                if(!cartDM.getProductPrice().isEmpty()){
+                    double newSubTotal = Double.parseDouble(cartDM.getProductPrice()) * count;
+
+                }
+                count = Integer.parseInt(binding.productCartQTY.getText().toString());
+                cartDM.setProductQuantity(count);
+
+            });
+
+
+        }
+    }
+}
--- app/src/main/java/com/ramez/shopp/Fragments/InvoiceFragment.java
+++ app/src/main/java/com/ramez/shopp/Fragments/InvoiceFragment.java
@@ -1,42 +1,64 @@
 package com.ramez.shopp.Fragments;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ramez.shopp.Adapter.InvoiceItemAdapter;
 import com.ramez.shopp.Adapter.PaymentAdapter;
+import com.ramez.shopp.Classes.CartModel;
 import com.ramez.shopp.Models.PaymentModel;
 import com.ramez.shopp.databinding.FragmentInvoiceBinding;
 
 import java.util.ArrayList;
 
-public class InvoiceFragment extends FragmentBase implements PaymentAdapter.OnPaymentClick {
+public class InvoiceFragment extends FragmentBase implements PaymentAdapter.OnPaymentClick,InvoiceItemAdapter.OnInvoiceItemClicked {
     private FragmentInvoiceBinding binding;
 
     private PaymentAdapter paymentAdapter;
     ArrayList<PaymentModel> paymentList;
     LinearLayoutManager payLinearLayoutManager;
 
+    private InvoiceItemAdapter invoiceProductAdapter;
+    ArrayList<CartModel> productList;
+    LinearLayoutManager linearLayoutManager;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentInvoiceBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
         paymentList=new ArrayList<>();
+        productList =new ArrayList<>();
+
         paymentList.add(new PaymentModel(1,"بطاقة ائتمان","بطاقة ائتمان"));
         paymentList.add(new PaymentModel(1,"بطاقة ائتمان","بطاقة ائتمان"));
         paymentList.add(new PaymentModel(1,"بطاقة ائتمان","بطاقة ائتمان"));
+
+
+        productList.add(new CartModel(1,2,1,2,0,10,"شيبس ليز الملح 25 جرام","https://www.supermama.me/system/App/Entities/Article/images/000/065/491/web-watermarked-large/%D9%85%D8%B4%D8%B1%D9%88%D8%A8%D8%A7%D8%AA-%D8%AD%D8%A7%D8%B1%D9%82%D8%A9-%D9%84%D9%84%D8%AF%D9%87%D9%88%D9%86.jpg",
+                "10","",0,"20",100));
+        productList.add(new CartModel(1,2,1,2,0,10,"شيبس ليز الملح 25 جرام","https://www.supermama.me/system/App/Entities/Article/images/000/065/491/web-watermarked-large/%D9%85%D8%B4%D8%B1%D9%88%D8%A8%D8%A7%D8%AA-%D8%AD%D8%A7%D8%B1%D9%82%D8%A9-%D9%84%D9%84%D8%AF%D9%87%D9%88%D9%86.jpg",
+                "10","",0,"20",100));
+        productList.add(new CartModel(1,2,1,2,0,10,"شيبس ليز الملح 25 جرام","https://www.supermama.me/system/App/Entities/Article/images/000/065/491/web-watermarked-large/%D9%85%D8%B4%D8%B1%D9%88%D8%A8%D8%A7%D8%AA-%D8%AD%D8%A7%D8%B1%D9%82%D8%A9-%D9%84%D9%84%D8%AF%D9%87%D9%88%D9%86.jpg",
+                "10","",0,"20",100));
+        productList.add(new CartModel(1,2,1,2,0,10,"شيبس ليز الملح 25 جرام","https://www.supermama.me/system/App/Entities/Article/images/000/065/491/web-watermarked-large/%D9%85%D8%B4%D8%B1%D9%88%D8%A8%D8%A7%D8%AA-%D8%AD%D8%A7%D8%B1%D9%82%D8%A9-%D9%84%D9%84%D8%AF%D9%87%D9%88%D9%86.jpg",
+                "10","",0,"20",100));
+        productList.add(new CartModel(1,2,1,2,0,10,"شيبس ليز الملح 25 جرام","https://www.supermama.me/system/App/Entities/Article/images/000/065/491/web-watermarked-large/%D9%85%D8%B4%D8%B1%D9%88%D8%A8%D8%A7%D8%AA-%D8%AD%D8%A7%D8%B1%D9%82%D8%A9-%D9%84%D9%84%D8%AF%D9%87%D9%88%D9%86.jpg",
+                "10","",0,"20",100));
+
 
         payLinearLayoutManager=new LinearLayoutManager(getActivityy(), RecyclerView.HORIZONTAL,false);
         binding.paymentRv.setLayoutManager(payLinearLayoutManager);
 
-
+        linearLayoutManager=new LinearLayoutManager(getActivityy());
+        binding.productsRecycler.setLayoutManager(linearLayoutManager);
+        binding.productsRecycler.setHasFixedSize(true);
 
 
         initAdapter();
@@ -48,10 +70,18 @@ public class InvoiceFragment extends FragmentBase implements PaymentAdapter.OnPa
 
         paymentAdapter = new PaymentAdapter(getActivityy(), paymentList,this);
         binding.paymentRv.setAdapter(paymentAdapter);
+
+        invoiceProductAdapter=new InvoiceItemAdapter(getActivityy(), productList,this);
+        binding.productsRecycler.setAdapter(invoiceProductAdapter);
     }
 
     @Override
     public void onPaymentClicked(int position, PaymentModel paymentModel) {
+
+    }
+
+    @Override
+    public void onInvoiceItemClicked(CartModel cartDM) {
 
     }
 }
